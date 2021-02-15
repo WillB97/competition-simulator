4ee69e13403b70dd6328d4e4188bd766ba5a22fb
Will Barber <WillB97@users.noreply.github.com>
2021-02-15 12:20:40 +0000

    BODGE: trial at 720p

--- modules/controller_utils/__init__.py
+++ modules/controller_utils/__init__.py
@@ -138,7 +138,7 @@ def get_recording_config() -> RecordingConfig:
     )
     if config is None:
         config = RecordingConfig(
-            Resolution(854, 480),
+            Resolution(1280, 720),
             quality=100,
         )
     return config
